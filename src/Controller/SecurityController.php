0e216d4ac9e2bc69f00d02c408038b15ff951582
alexandre <alexanregiannetto@thenetgroup.net>
2019-06-26 20:01:46 +0200

    creation security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+}
