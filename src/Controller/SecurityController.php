854529e6e4e0b2999c0af43210bfdf004a436698
alexandre <alexanregiannetto@thenetgroup.net>
2019-06-26 20:32:06 +0200

    login function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\LoginType;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
@@ -30,6 +35,21 @@ class SecurityController extends AbstractController
         }
 
         return $this->render('security/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $user = new User();
+        $user->setEmail($authenticationUtils->getLastUsername());
+        $form = $this->createForm(LoginType::class, $user);
+
+        return $this->render('security/index.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError(),
             'form' => $form->createView()
         ]);
     }
