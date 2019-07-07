a067bf06dcba6dab23e19a84b4839d612981ad06
Alexg78bis <alexg78bis@gmail.com>
2019-07-08 00:23:09 +0200

    - rename vehicule into vehicle - rename few field into vehicle entity - add relation between Vehicle and VehicleType - add new field into vehicle

---
+++ src/Entity/VehicleType.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\VehicleTypeRepository")
+ */
+class VehicleType
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Vehicle", mappedBy="vehicleType", orphanRemoval=true)
+     */
+    private $vehicles;
+
+
+    public function __construct()
+    {
+        $this->vehicles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Vehicle[]
+     */
+    public function getVehicles(): Collection
+    {
+        return $this->vehicles;
+    }
+
+    public function addVehicle(Vehicle $vehicle): self
+    {
+        if (!$this->vehicles->contains($vehicle)) {
+            $this->vehicles[] = $vehicle;
+            $vehicle->setVehicleType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVehicle(Vehicle $vehicle): self
+    {
+        if ($this->vehicles->contains($vehicle)) {
+            $this->vehicles->removeElement($vehicle);
+            // set the owning side to null (unless already changed)
+            if ($vehicle->getVehicleType() === $this) {
+                $vehicle->setVehicleType(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+}
