83094c0d8b363f6019f945c5006da63affb41a8e
Enzo <enzo.borges@hotmail.fr>
2019-07-24 12:57:19 +0200

    Add method to send mail with zozotueur@gmail.com to zozotueur@gmail.com. Contact method, use first name, last name, mail, message, object

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -25,5 +28,40 @@ class DefaultController extends AbstractController
         return $this->render('default/services.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(Request $request, \Swift_Mailer $mailer)
+    {
+        if (!empty($request->request->get('object'))) {
+            $object = $request->request->get('object');
+            $firstNameContact = $request->request->get('firstNameContact');
+            $lastNameContact = $request->request->get('lastNameContact');
+            $emailContact = $request->request->get('emailContact');
+            $messageContact = $request->request->get('message');
+            $message = (new \Swift_Message('Contact : ' . $object))
+                ->setFrom('zozotueur@gmail.com')
+                ->setTo('zozotueur@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                    // templates/emails/registration.html.twig
+                        'emails/registration.html.twig',
+                        [
+                            'firstNameContact' => $firstNameContact,
+                            'lastNameContact' => $lastNameContact,
+                            'emailContact' => $emailContact,
+                            'messageContact' => $messageContact
+                        ]
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+            return $this->render('default/validationMail.html.twig');
+        } else {
+            return $this->render('default/contact.html.twig');
+        }
     }
 }
