f48ea3d6620c91103bce09c25c84a7b970ab586f
Alexg78bis <alexg78bis@gmail.com>
2019-08-06 17:15:59 +0200

    - list + info of available vehicle

--- src/Controller/RentalController.php
+++ src/Controller/RentalController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CarDealer;
+use App\Entity\Rental;
+use App\Entity\Vehicle;
 use App\Repository\CarDealerRepository;
 use App\Repository\VehicleRepository;
 use App\Repository\VehicleTypeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,17 +18,17 @@ class RentalController extends AbstractController
     /**
      * @Route("/search", name="rental__search")
      */
-    public function index(Request $request, VehicleRepository $vehicleRepository, VehicleTypeRepository $vehicleTypeRepository, CarDealerRepository $carDealerRepository)
+    public function search(Request $request, VehicleRepository $vehicleRepository, VehicleTypeRepository $vehicleTypeRepository, CarDealerRepository $carDealerRepository)
     {
         $dateStart = $request->query->get('date_start');
         $dateEnd = $request->query->get('date_end');
         $idTypeVehicle = $request->query->get('type');
         $idLocation = $request->query->get('location');
 
-        $vehicules = $vehicleRepository->getAvailableVehicle($idTypeVehicle, $idLocation, $dateStart, $dateEnd);
+        $vehicles = $vehicleRepository->getAvailableVehicle($idTypeVehicle, $idLocation, $dateStart, $dateEnd);
 
         return $this->render('rental/index.html.twig', [
-            'vehicles' => $vehicules,
+            'vehicles' => $vehicles,
             'carDealer' => $carDealerRepository->findAll(),
             'carType' => $vehicleTypeRepository->findAll(),
             'dateStart' => $dateStart,
@@ -33,4 +37,34 @@ class RentalController extends AbstractController
             'idTypeVehicle' => $idTypeVehicle,
         ]);
     }
+
+
+    /**
+     * @Route("/overview/{dateStart}/{dateEnd}/{carDealer}/{vehicle}", name="rental__overview")
+     * @ParamConverter("carDealer", options={"id" = "carDealer"})
+     * @ParamConverter("vehicle", options={"id" = "vehicle"})
+     */
+    public function overview(string $dateStart, string $dateEnd, CarDealer $carDealer, Vehicle $vehicle, VehicleRepository $vehicleRepository)
+    {
+
+        $availableVehicleForSelectedDate = $vehicleRepository->getAvailableVehicle($vehicle->getVehicleType()->getId(), $carDealer->getId(), $dateStart, $dateEnd);
+
+        if (!in_array($vehicle, $availableVehicleForSelectedDate)) {
+            return $this->redirectToRoute('home');
+        }
+
+        $rental = new Rental();
+        $rental->setClient($this->getUser());
+        $rental->setVehicle($vehicle);
+        $rental->setStartRentalDate(new \DateTime($dateStart));
+        $rental->setEstimatedReturnDate(new \DateTime($dateEnd));
+        $rental->setPrice($vehicle->getDailyPrice());
+
+        return $this->render('rental/overview.html.twig', [
+            'rental' => $rental
+        ]);
+
+    }
+
+
 }
